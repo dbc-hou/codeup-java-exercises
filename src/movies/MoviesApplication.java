fdb625e1857ad201afed9d95f701772bb5034dbe
dbc-hou <david.collins.hou@gmail.com>
2022-03-10 17:01:59 -0600

    array exercises more-or-less completed

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -3,50 +3,60 @@ package movies;
 import util.Input;
 
 public class MoviesApplication {
-    public static void main(String[] args) {
-        MoviesArray mArray = new MoviesArray();
+    public static void main(String[] args) throws InterruptedException {
+        Movie[] mArray = MoviesArray.findAll();
         Input i = new Input();
-        System.out.println("What would you like to do?");
-        System.out.println("0 - Exit");
-        System.out.println("1 - View all movies");
-        System.out.println("View movies in a specific category:");
-        System.out.println("2 - animated");
-        System.out.println("3 - comedy");
-        System.out.println("4 - drama");
-        System.out.println("5 - horror");
-        System.out.println("6 - musical");
-        System.out.println("7 - scifi");
-        String categorySelected;
-        String userChoice = i.getString();
-        switch userChoice {
-            case 0:
-                i.closeMe();
-                break;
-            case 1:
-                for (Movie m : mArray.findAll) {
-                    System.out.println(m.name + " -- " + m.category);
-                }
-                break;
-            case 2:
-                categorySelected = "animated";
-                break;
-            case 3:
-                categorySelected = "comedy";
-                break;
-            case 4:
-                categorySelected = "drama";
-                break;
-            case 5:
-                categorySelected = "horror";
-                break;
-            case 6:
-                categorySelected = "musical";
-                break;
-            case 7:
-                categorySelected = "scifi";
-                break;
-            default:
+        String userChoice = null;
+        while (userChoice != "0") {
+            System.out.println("What would you like to do?");
+            System.out.println("0 - Exit");
+            System.out.println("1 - View all movies");
+            System.out.println("View movies in a specific category:");
+            System.out.println("2 - animated");
+            System.out.println("3 - comedy");
+            System.out.println("4 - drama");
+            System.out.println("5 - horror");
+            System.out.println("6 - musical");
+            System.out.println("7 - scifi");
+            System.out.println();
+            System.out.println("Enter your choice:");
+            userChoice = i.getString();
+            String categorySelected = "";
 
+            switch (userChoice) {
+                case "0":
+                    System.exit(0);
+                case "1":
+                    categorySelected = "";
+                    break;
+                case "2":
+                    categorySelected = "animated";
+                    break;
+                case "3":
+                    categorySelected = "comedy";
+                    break;
+                case "4":
+                    categorySelected = "drama";
+                    break;
+                case "5":
+                    categorySelected = "horror";
+                    break;
+                case "6":
+                    categorySelected = "musical";
+                    break;
+                case "7":
+                    categorySelected = "scifi";
+                    break;
+                default:
+            }
+            for (Movie m : mArray) {
+                if (m.getCategory().equalsIgnoreCase(categorySelected) || categorySelected.length() == 0) {
+                    System.out.println(m.getName() + " -- " + m.getCategory());
+                }
+            }
+            System.out.println();
+            Thread.sleep(2000);
         }
+
     }
 }
