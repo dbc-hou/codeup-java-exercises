817f074f7de1b0d5ffd5fdf99c235fef264686dd
dbc-hou <david.collins.hou@gmail.com>
2022-03-11 09:41:08 -0600

    array exercises refactored

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -7,56 +7,62 @@ public class MoviesApplication {
         Movie[] mArray = MoviesArray.findAll();
         Input i = new Input();
         String userChoice = null;
-        while (userChoice != "0") {
-            System.out.println("What would you like to do?");
-            System.out.println("0 - Exit");
-            System.out.println("1 - View all movies");
-            System.out.println("View movies in a specific category:");
-            System.out.println("2 - animated");
-            System.out.println("3 - comedy");
-            System.out.println("4 - drama");
-            System.out.println("5 - horror");
-            System.out.println("6 - musical");
-            System.out.println("7 - scifi");
-            System.out.println();
+        do {
+            printMenu();
             System.out.println("Enter your choice:");
             userChoice = i.getString();
-            String categorySelected = "";
-
-            switch (userChoice) {
-                case "0":
-                    System.exit(0);
-                case "1":
-                    categorySelected = "";
-                    break;
-                case "2":
-                    categorySelected = "animated";
-                    break;
-                case "3":
-                    categorySelected = "comedy";
-                    break;
-                case "4":
-                    categorySelected = "drama";
-                    break;
-                case "5":
-                    categorySelected = "horror";
-                    break;
-                case "6":
-                    categorySelected = "musical";
-                    break;
-                case "7":
-                    categorySelected = "scifi";
-                    break;
-                default:
-            }
-            for (Movie m : mArray) {
-                if (m.getCategory().equalsIgnoreCase(categorySelected) || categorySelected.length() == 0) {
-                    System.out.println(m.getName() + " -- " + m.getCategory());
-                }
-            }
+            doUserChoice(userChoice,mArray);
             System.out.println();
             Thread.sleep(2000);
-        }
+        } while (userChoice != "0");
+    }
 
+    private static void printMenu() {
+        System.out.println("What would you like to do?");
+        System.out.printf("\t0 - Exit\n");
+        System.out.printf("\t1 - View all movies\n");
+        System.out.println("View movies in a specific category:");
+        System.out.printf("\t2 - animated\n");
+        System.out.printf("\t3 - comedy\n");
+        System.out.printf("\t4 - drama\n");
+        System.out.printf("\t5 - horror\n");
+        System.out.printf("\t6 - musical\n");
+        System.out.printf("\t7 - scifi\n");
+        System.out.println();
+    }
+
+    private static void doUserChoice(String userChoice, Movie[] movies) {
+        String categorySelected = "";
+        switch (userChoice) {
+            case "0":
+                System.exit(0);
+            case "1":
+                categorySelected = "";
+                break;
+            case "2":
+                categorySelected = "animated";
+                break;
+            case "3":
+                categorySelected = "comedy";
+                break;
+            case "4":
+                categorySelected = "drama";
+                break;
+            case "5":
+                categorySelected = "horror";
+                break;
+            case "6":
+                categorySelected = "musical";
+                break;
+            case "7":
+                categorySelected = "scifi";
+                break;
+            default:
+        }
+        for (Movie m : movies) {
+            if (m.getCategory().equalsIgnoreCase(categorySelected) || categorySelected.length() == 0) {
+                System.out.println(m.getName() + " -- " + m.getCategory());
+            }
+        }
     }
 }
