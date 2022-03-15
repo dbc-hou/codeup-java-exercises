c93002ce45903ab218934cabe3d05afb1a49241f
dbc-hou <david.collins.hou@gmail.com>
2022-03-15 11:57:45 -0500

    exceptions exercises, with bonus

--- src/util/Input.java
+++ src/util/Input.java
@@ -12,6 +12,7 @@ public class Input {
     public String getString() {
         return scanner.nextLine();
     }
+
     public boolean yesNo() {
         String scInput = scanner.nextLine();
         if (scInput.equalsIgnoreCase("y") || scInput.equalsIgnoreCase("yes") || scInput.equalsIgnoreCase("true")) {
@@ -22,7 +23,13 @@ public class Input {
     }
 
     public int getInt(int min, int max) {
-        int scIntInput = scanner.nextInt();
+        int scIntInput = 0;
+        try {
+            scIntInput = Integer.valueOf(getString());
+        } catch(Exception e) {
+            System.out.println("Sorry, that isn't an integer.");
+            getInt(min,max);
+        }
         if (scIntInput < min || scIntInput > max) {
             System.out.println("Please follow the instructions and enter an integer.");
             getInt(min, max);
@@ -32,12 +39,24 @@ public class Input {
         return 0;
     }
 
-    public int getInt() {
-        return scanner.nextInt();
+    public int getInt(String prompt) {
+        try {
+            System.out.println(prompt);
+            return Integer.valueOf(getString());
+        } catch (Exception e){
+            System.out.println("Sorry, that isn't an integer.");
+            return getInt(prompt);
+        }
     }
 
     public double getDouble(double min, double max) {
-        double scDblInput = scanner.nextDouble();
+        double scDblInput = 0;
+        try {
+            scDblInput = Double.valueOf(getString());
+        } catch (Exception e) {
+            System.out.println("Sorry, that isn't an number.");
+            getDouble(min,max);
+        }
         if (scDblInput < min || scDblInput > max) {
             System.out.println("Please follow the instructions and enter something numeric.");
             getDouble(min,max);
@@ -47,10 +66,35 @@ public class Input {
         return 0;
     }
 
-    public double getDouble() {
-        return scanner.nextDouble();
+    public double getDouble(String prompt) {
+        try {
+            System.out.println(prompt);
+            return Double.valueOf(getString());
+        } catch (Exception e) {
+            System.out.println("Sorry, we need a positive number, decimals are OK.");
+            return getDouble(prompt);
+        }
+
+    }
+    public int getBinary(String prompt) {
+        try {
+            System.out.println(prompt);
+            return Integer.valueOf(getString(),2);
+        } catch (Exception e) {
+            System.out.println("Sorry, that doesn't work. Please try again, 0's and 1's only.");
+            return getBinary(prompt);
+        }
     }
 
+    public int getHex(String prompt) {
+        try {
+            System.out.println(prompt);
+            return Integer.valueOf(getString(),16);
+        } catch (Exception e) {
+            System.out.println("Sorry, that doesn't work. Enter all digits or A through F.");
+            return getHex(prompt);
+        }
+    }
     public void closeMe() {
         scanner.close();
     }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -5,25 +5,32 @@ import java.util.Scanner;
 public class InputTest {
     public static void main(String[] args) {
         Input myInput = new Input();
-        System.out.println("What's your name, user?");
-        String yourName = myInput.getString();
-        System.out.printf("Welcome, %s!",yourName);
+//        System.out.println("What's your name, user?");
+//        String yourName = myInput.getString();
+//        System.out.printf("Welcome, %s!",yourName);
+//        System.out.println();
+//        System.out.println("Are you enjoying your web development training?");
+//        boolean enjoying = myInput.yesNo();
+//        if (enjoying) {
+//            System.out.println("I'm glad to know that!");
+//        } else {
+//            System.out.println("Maybe it will go better in the weeks to come.");
+//        }
+//        System.out.println();
         System.out.println();
-        System.out.println("Are you enjoying your web development training?");
-        boolean enjoying = myInput.yesNo();
-        if (enjoying) {
-            System.out.println("I'm glad to know that!");
-        } else {
-            System.out.println("Maybe it will go better in the weeks to come.");
-        }
-        System.out.println();
-        System.out.println("What's your age in years? (Integer only, please.)");
-        int yourAge = myInput.getInt();
+        int yourAge = myInput.getInt("What's your age in years? (Integer only, please.)");
         System.out.println("Cool. The square root of that is " + Math.sqrt(yourAge) + ".");
-        System.out.println("Last question: Enter any positive value for the radius of a circle.");
-        double yourRadius = myInput.getDouble();
+        System.out.println();
+        int myBinaryConversion = myInput.getBinary("Let's convert binary numbers to decimal! Enter a string of 0's and 1's:");
+        System.out.println("That converts to " + myBinaryConversion + " in base 10.");
+        System.out.println();
+        int myHexConversion = myInput.getHex("Now we'll try hexadecimal to decimal. Enter a hexadecimal number (only characters allowed are 0 through 9 and A through F):");
+        System.out.println("That converts to " + myHexConversion + " in base 10.");
+        System.out.println();
+        double yourRadius = myInput.getDouble("Last question: Enter any positive value for the radius of a circle.");
         System.out.println("The circumference of a circle with radius " + yourRadius + " is " + Math.PI * 2 * yourRadius + ".");
         System.out.println("The area of a circle with radius " + yourRadius + " is " + Math.PI * yourRadius * yourRadius + ".");
+        System.out.println("All done! I enjoyed playing with numbers—and with you!");
         myInput.closeMe();
     }
 }
